d30a2026fe885fe7d6ccc4c6da19615bec4f9c37
jspiner <jspiner@naver.com>
2018-12-22 23:20:37 +0900

    generic 문제때문에 internal 함수로 viewholder 를 생성하도록 변경

--- src/main/java/net/jspiner/ask/ui/base/BaseAdapter.kt
+++ src/main/java/net/jspiner/ask/ui/base/BaseAdapter.kt
@@ -2,15 +2,18 @@ package net.jspiner.ask.ui.base;
 
 import android.databinding.ViewDataBinding
 import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
 import java.util.ArrayList
 
 abstract class BaseAdapter<D> : RecyclerView.Adapter<BaseViewHolder<ViewDataBinding, D>>() {
 
     private val dataList: ArrayList<D> = ArrayList()
 
-    override fun onBindViewHolder(holder: BaseViewHolder<ViewDataBinding, D>, position: Int) {
-        holder.setData(dataList[position])
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<ViewDataBinding, D> {
+        return onCreateViewHolderInternal(parent, viewType) as BaseViewHolder<ViewDataBinding, D>
     }
+
+    abstract fun onCreateViewHolderInternal(parent: ViewGroup, viewType: Int): BaseViewHolder<out ViewDataBinding,D>
 
     override fun getItemCount(): Int {
         return dataList.size
